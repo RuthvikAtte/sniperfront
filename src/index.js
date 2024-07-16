96bbb790c924ced19698d67780f23aff1187af19
RuthvikAtte <147272241+RuthvikAtte@users.noreply.github.com>
2024-07-16 17:50:01 -0400

    With React Dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/App';
+import * as ReactDOM from "react-dom/client";
+import ContactPage from './pages/ContactPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>
+  }
+,
+  {
+    path: "contact",
+    element: <ContactPage></ContactPage>
+  }
+
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
