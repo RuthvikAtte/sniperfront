49cfcc429e419f9dc394d5f5b21f10565048c192
Anish Jha <anishj2900@gmail.com>
2024-07-23 01:15:15 -0400

    test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ReactTyped} from 'react-typed'
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 
 export default function Contact() {
   return (
