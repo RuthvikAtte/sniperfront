2ca04b35eff7c1fbdf40a3046ebdb617552263bc
Anish Jha <anishj2900@gmail.com>
2024-08-04 22:49:59 -0400

    connect email-js service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,49 @@
-import React from 'react'
-import { ReactTyped} from 'react-typed'
-// import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react'
+// import { ReactTyped} from 'react-typed'
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_p07nxzi', 'template_zcmafcc', form.current, {
+        publicKey: 'aNG9MNGX9o5wyqTL4',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
-    <div className='h-screen text-white contact-bg'>
+    <form className='h-screen text-white contact-bg' ref={form} onSubmit={sendEmail}>
       <h1 className='text-3xl contact-title'>Contact Us</h1>
       <div className='mb-4'>
         <h3 className='mb-2'>Name</h3>
         <div>
-          <input name="name" className='st-box' placeholder="Name"  />
+          <input name="from_name" className='st-box' placeholder="Name"  />
         </div>
       </div>
       <div className='mb-4'>
         <h3 className='mb-2'>Email</h3>
         <div>
-          <input name="email" className='st-box' placeholder="jane@example.com"  />
+          <input name="user_email" className='st-box' placeholder="jane@example.com"  />
         </div>
       </div>
       <div className='mb-4'>
         <h3 className='mb-2'>Message</h3>
         <div>
-          <textarea type="text" class="cln" className='bt-box' placeholder="Message" col={10} rows={5} />
+          <textarea name="message" type="text" class="cln" className='bt-box' placeholder="Message" col={10} rows={5} />
         </div>
       </div>
-      <button className='contact-btn shadow-lg'>SUBMIT</button>
-    </div>
-  )
+      <button type="submit" className='contact-btn shadow-lg'>SUBMIT</button>
+    </form>
+  );
 }
