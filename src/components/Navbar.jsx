c0fcb69339b2876118e300db2c556202dd860782
Anish Jha <anishj2900@gmail.com>
2024-08-07 22:56:41 -0400

    temp navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
     }
   return (
     <div className = 'flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white navbar-bg'>
-      <h1 className='w-full text-3xl font-bold red-theme'>RU TURBO</h1>
-        <ul className='hidden md:flex'>
+      <h1 className='w-full text-3xl font-bold red-theme navbar-title'>RU TURBO</h1>
+        <ul className='md:flex'>
         <button className='p-4'><Link to="/">Home</Link></button>
         <button className='p-4'><Link to="/contact">Contact</Link></button>
         </ul>
