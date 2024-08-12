49db917f0945e6a3337291a79d1a8bc93adba1de
Anish Jha <anishj2900@gmail.com>
2024-08-12 13:09:19 -0400

    red spline bg

--- src/components/SplineScene.jsx
+++ src/components/SplineScene.jsx
@@ -1,32 +1,28 @@
-import React, { useEffect } from 'react';
-import Spline from '@splinetool/react-spline';
+import React, { useEffect } from "react";
+import Spline from "@splinetool/react-spline";
 
-const SplineScene = ({onLoaded}) => {
+const SplineScene = ({ onLoaded }) => {
+  useEffect(() => {
+    // Simulate spline component loading
+    const timer = setTimeout(() => {
+      if (onLoaded) {
+        onLoaded();
+      }
+    }, 3000); // Adjust this timing based on your actual loading time
 
-    useEffect(() => {
-        // Simulate spline component loading
-        const timer = setTimeout(() => {
-          if (onLoaded) {
-            onLoaded();
-          }
-        }, 3000); // Adjust this timing based on your actual loading time
-    
-        return () => clearTimeout(timer);
-      }, [onLoaded]);
+    return () => clearTimeout(timer);
+  }, [onLoaded]);
 
-      
-    return (
-      //   <div className="spline-background">
-      //       <Spline
-      //   scene="https://prod.spline.design/4gEOJhKN116u6bHE/scene.splinecode" 
-      // />
-      //   </div>
-        <div className="spline-background">
-        <Spline
-    scene="https://prod.spline.design/8ciRUqlhwaU5bKsp/scene.splinecode" 
-  />
+  return (
+    //   <div className="spline-background">
+    //       <Spline
+    //   scene="https://prod.spline.design/4gEOJhKN116u6bHE/scene.splinecode"
+    // />
+    //   </div>
+    <div className="spline-background">
+      <Spline scene="https://prod.spline.design/8ciRUqlhwaU5bKsp/scene.splinecode" />
     </div>
-    );
+  );
 };
 
 export default SplineScene;
